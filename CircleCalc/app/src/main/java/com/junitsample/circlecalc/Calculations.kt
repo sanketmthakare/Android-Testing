b2b0fd1d4ed3c14d2802a02a7abb0ab994ad6a42
SANKET\sanke <sankett@vobium.net>
2023-08-20 23:47:27 +0530

    Android JUnit Sample done

---
+++ CircleCalc/app/src/main/java/com/junitsample/circlecalc/Calculations.kt
@@ -0,0 +1,7 @@
+package com.junitsample.circlecalc
+
+interface Calculations {
+
+    fun calculateCircumference(radius:Double) : Double
+    fun calculateArea(radius:Double) : Double
+}
---
+++ CircleCalc/app/src/test/java/com/junitsample/circlecalc/MyCalcTest.kt
@@ -0,0 +1,45 @@
+package com.junitsample.circlecalc
+
+
+import com.google.common.truth.Truth8.assertThat
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+
+
+import org.junit.Test
+
+
+class MyCalcTest {
+    private lateinit var myCalc: MyCalc
+
+
+    @Before
+    fun setUp() {
+        myCalc = MyCalc()
+    }
+
+    @Test
+    fun calculateCircumference_radiusGiven_returnsCorrectResult() {
+
+        val result = myCalc.calculateCircumference(2.1)
+        assertThat(result).isEqualTo(13.188)
+    }
+
+    @Test
+    fun calculateCircumference_zeroRadius_returnsCorrectResult() {
+        val result = myCalc.calculateCircumference(0.0)
+        assertThat(result).isEqualTo(0)
+    }
+
+    @Test
+    fun calculateArea_radiusGiven_returnCorrectResult() {
+        val result = myCalc.calculateArea(2.1)
+        assertThat(result).isEqualTo(13.8474)
+    }
+
+    @Test
+    fun calculateArea_zeroRadius_returnCorrectResult() {
+        val result = myCalc.calculateArea(0.0)
+        assertThat(result).isEqualTo(0)
+    }
+}
